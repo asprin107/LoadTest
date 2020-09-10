f532c4ef8fa3993cd7563cb5fa53cb6330afd059
ec.asprin <ec.asprin@kalmate.net>
2020-09-10 16:18:11 +0900

    edit test duration and run-test script

--- src/main/resources/taurus/scripts/run-test.sh
+++ src/main/resources/taurus/scripts/run-test.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-LOAD_GENERATORS=$(
-  aws ecs describe-tasks --cluster awsdc-ecl-pss-dev-mgr --tasks "$(
-    aws ecs list-tasks --cluster awsdc-ecl-pss-dev-mgr --family awsdc-etk-pss-dev-load-test | \
-    jq -r .taskArns[]
-  )" | jq '.tasks[].attachments[].details[] | select(.name == "privateIPv4Address")' | \
-  jq -r .value
-)
+LOAD_GENERATORS=$(aws ecs describe-tasks --cluster awsdc-ecl-pss-dev-mgr --tasks $(aws ecs list-tasks --cluster awsdc-ecl-pss-dev-mgr --family awsdc-etk-pss-dev-load-test | jq -r .taskArns[]) | jq '.tasks[].attachments[].details[] | select(.name == "privateIPv4Address")' | jq -r .value)
 
-LOAD_GENERATORS_ARR=("$(echo ${LOAD_GENERATORS} | tr ' ' "\n")")
+LOAD_GENERATORS_ARR=($(echo ${LOAD_GENERATORS} | tr '  ' "\n"))
 
+echo "############### health check ###############"
 for LOAD_SERVER in "${LOAD_GENERATORS_ARR[@]}"; do
-  curl http://${LOAD_SERVER}/isAlive &
+  echo "${LOAD_SERVER}:"
+  curl http://${LOAD_SERVER}:7080/isAlive
+  echo ""
 done
 
-#for LOAD_SERVER in "${LOAD_GENERATORS_ARR[@]}"; do
-#  curl http://${LOAD_SERVER}/init &
-#done
+echo "############### init load test ###############"
+for LOAD_SERVER in "${LOAD_GENERATORS_ARR[@]}"; do
+  echo "${LOAD_SERVER}:"
+  curl http://${LOAD_SERVER}:7080/init
+  echo ""
+done
 
 #for LOAD_SERVER in "${LOAD_GENERATORS_ARR[@]}"; do
 #  curl http://${LOAD_SERVER}/actuator/shutdown
